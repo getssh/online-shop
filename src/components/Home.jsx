835b8dd2021aa34666742121b93524b4fd57630c
getssh <gtamrat33@gmail.com>
2023-05-06 02:33:34 +0300

    Add grid to homepage products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,21 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getHome } from '../redux/homeSlice';
 import { useEffect, useState } from 'react';
 
+{/* <div>
+<div className="flex">
+  <button onClick={() => slide <= 0 ? setSlide(product.images.length - 1) : setSlide(slide - 1)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+    </svg>
+  </button>
+  <img src={product.images[slide]}/>
+  <button onClick={() => slide < product.images.length - 1 ? setSlide(slide + 1) : setSlide(0)}>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+      <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+    </svg>
+  </button>
+</div>
+</div> */}
 const Home = () => {
   const { products } = useSelector((state) => state.products)
   const dispatch = useDispatch();
@@ -11,39 +26,22 @@ const Home = () => {
   }, [])
 
   console.log(products)
-  const [slide, setSlide] = useState(0)
-
-  const handelSlide = (images) => {
-    return (
-      <div className="flex">
-        <button onClick={() => slide <= 0 ? setSlide(images.length - 1) : setSlide(slide - 1)}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
-          </svg>
-        </button>
-        <img src={images[slide]}/>
-        <button onClick={() => slide < images.length - 1 ? setSlide(slide + 1) : setSlide(0)}>
-          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
-            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
-          </svg>
-        </button>
-      </div>
-    )
-  }
   return (
     <div>
       {
         products.products &&
-        <div>
+        <div className='grid grid-cols-4 gap-4'>
           {
             products.products.map((product, index) => {
               return (
                 <div key={product.id}>
-                  <div>
-                    {handelSlide(product.images)}
-                  </div>
+                  <img src={product.thumbnail}/>
                   <p>{product.title}</p>
                   <p>{product.description}</p>
+                  <p className='text-[#f00]'>-{product.discountPercentage}%</p>
+                  <p>${product.price}</p>
+                  <p>{product.rating}</p>
+                  <p>{product.category}</p>
                 </div>
               )
             })
