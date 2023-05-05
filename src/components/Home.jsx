388526a999ea2d6b7fb0b2766750cf25a06051a0
getssh <gtamrat33@gmail.com>
2023-05-06 02:16:57 +0300

    Add image slide for products

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import { useSelector, useDispatch } from 'react-redux';
 import { getHome } from '../redux/homeSlice';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 const Home = () => {
   const { products } = useSelector((state) => state.products)
@@ -11,6 +11,25 @@ const Home = () => {
   }, [])
 
   console.log(products)
+  const [slide, setSlide] = useState(0)
+
+  const handelSlide = (images) => {
+    return (
+      <div className="flex">
+        <button onClick={() => slide <= 0 ? setSlide(images.length - 1) : setSlide(slide - 1)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+          </svg>
+        </button>
+        <img src={images[slide]}/>
+        <button onClick={() => slide < images.length - 1 ? setSlide(slide + 1) : setSlide(0)}>
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+          </svg>
+        </button>
+      </div>
+    )
+  }
   return (
     <div>
       {
@@ -20,6 +39,9 @@ const Home = () => {
             products.products.map((product, index) => {
               return (
                 <div key={product.id}>
+                  <div>
+                    {handelSlide(product.images)}
+                  </div>
                   <p>{product.title}</p>
                   <p>{product.description}</p>
                 </div>
