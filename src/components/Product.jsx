e71db3769989b6e6e58a3a37f13f146b7f4a1072
getssh <gtamrat33@gmail.com>
2023-05-23 11:26:03 +0300

    Update product image to be fixed

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -28,7 +28,7 @@ const Product = () => {
                   <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
                 </svg>
               </button>
-                  <img className="h-80 w-auto overflow-hiden p-4" src={product.images[slide]}/>
+                  <img className="w-96 h-96 overflow-hiden p-4" src={product.images[slide]}/>
               <button onClick={() => slide < product.images.length - 1 ? setSlide(slide + 1) : setSlide(0)}>
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
