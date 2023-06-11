fd964260a06cdc86280799dde2ab816267c21bc7
getssh <gtamrat33@gmail.com>
2023-06-11 22:50:22 +0300

    Updte filter method

--- src/redux/cartSlice.jsx
+++ src/redux/cartSlice.jsx
@@ -23,7 +23,7 @@ const cartSlice = createSlice(
       },
       removeItem: (state, action) => {
         state.items = state.items.filter((item) => {
-          return item.id !== action.payload;
+          return item.id == action.payload;
         })
       }
     }
